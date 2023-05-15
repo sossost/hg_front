4d1c115681a7a2e4d08a002cbe96f4976f0b1b06
[SW4/1반] 이수현 <happyshsh72@gmail.com>
2023-05-16 00:38:55 +0900

    Feat : 로그인 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import Editor from "./components/UI/Editor";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
-import MobileFirstPage from "./components/MobileFirstPage";
-import InstagramGallery from "./components/MobileFirstPage";
-import ReactForm from "./components/ReactForm";
+// import MobileFirstPage from "./components/MobileFirstPage";
+// import InstagramGallery from "./components/MobileFirstPage";
+// import ReactForm from "./components/ReactForm";
+import LoginPage from "./pages/auth/LoginPage";
 
 function App() {
   return (
     <Layout>
       <Router>
+        {/* <Routes>
+          <Route path="/" element={<ReactForm />} />
+        </Routes> */}
+
         <Routes>
-          <Route path="/" Component={ReactForm} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
       </Router>
     </Layout>
