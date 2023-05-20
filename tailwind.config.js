733e9e244d5fd67b97bbef9f89d8345a69b78c2c
jang_yoonsu <shagrat8546@gmail.com>
2023-05-20 14:23:34 +0900

    Feat : 새 여행일기 작성, 수정 페이지 - 에디터 컴포넌트 - 에디터 제출 모달 컴포넌트 - 여행지 추출 컴포넌트 - 텍스트 인풋 컴포넌트 - 새 여행일기 작성 페이지 - 좌표 주소 변환 함수 - 에디터 css 파일

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slideUp: {
+          from: {
+            transform: "translateY(-50%)",
+          },
+          to: {
+            transform: "translateY(-100%)",
+          },
+        },
+        editorModalSlideUp: {
+          from: {
+            transform: "translateY(100px)",
+          },
+          to: {
+            transform: "translateY(0px)",
+          },
+        },
+      },
+      animation: {
+        slideUp: "slideUp 0.25s ease-out forwards;",
+        editorModalSlideUp: "editorModalSlideUp 0.25s ease-out forwards;",
+      },
+    },
   },
   plugins: [],
 };
