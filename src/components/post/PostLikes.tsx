08fcbbb2434cad026458cd8e3e1a7a7324a2c600
고병욱 <rhquddnr9502@gmail.com>
2023-05-22 16:40:29 +0900

    Test : 좋아요 컴포넌트 더미데이터 추가

--- src/components/post/PostLikes.tsx
+++ src/components/post/PostLikes.tsx
@@ -10,9 +10,26 @@ type PostLikesProps = {
   initialLikesCount?: number;
 };
 
+/** test 더미 데이터 */
+const post1 = {
+  isLike: true,
+  likedPersons: [
+    "김김",
+    "박박",
+    "이이이",
+    "정정정정",
+    "김김",
+    "박박",
+    "이이이",
+    "정정정정",
+  ],
+};
+
 /**좋아요 컴포넌트 */
-const PostLikes = ({ initialLikesCount = 13 }: PostLikesProps) => {
-  const [isLiked, setIsLiked] = useState(false);
+const PostLikes = ({
+  initialLikesCount = post1.likedPersons.length,
+}: PostLikesProps) => {
+  const [isLiked, setIsLiked] = useState(post1.isLike);
   const [likesCount, setLikesCount] = useState(initialLikesCount);
 
   const handleLikeButtonClick = () => {
