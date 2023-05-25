8709827d60b980187f72a5a7bd0e73c4fd1e4db4
고병욱 <rhquddnr9502@gmail.com>
2023-05-25 12:25:14 +0900

    Refactor : utils 함수내의 인자 네이밍 변경

--- src/utils/isLikes-function.ts
+++ src/utils/isLikes-function.ts
@@ -1,7 +1,7 @@
 /** 가져온 좋아요 유저 목록에 로그인 유저가 속해있는지 확인하는 함수 인자로 (likedPersons[], loginedId) 를 넣으면 true, false를 반환함 */
 export const isLikedFunction = (
-  post: { likedPersons: number[] },
+  arr: { likedPersons: number[] },
   loginedId: number
 ): boolean => {
-  return post.likedPersons.includes(loginedId);
+  return arr.likedPersons.includes(loginedId);
 };
