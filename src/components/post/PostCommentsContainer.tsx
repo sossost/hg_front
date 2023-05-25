de66140249982a93d931b022d96fd7d68e437031
고병욱 <rhquddnr9502@gmail.com>
2023-05-25 12:16:47 +0900

    Feat : 댓글 컴포넌트 기능 구현

--- src/components/post/PostCommentsContainer.tsx
+++ src/components/post/PostCommentsContainer.tsx
@@ -1,58 +1,60 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import { atom, useRecoilState } from "recoil";
 import PostLikes from "../UI/PostLikes";
 import Comments from "./comments/Comments";
+import { isLikedFunction } from "../../utils/isLikes-function";
 
-/** 댓글 이미지 */
+/** 댓글 이미지 스타일드 컴포넌트 */
 const CommentImage = styled.img`
   width: 25px;
   height: 25px;
   margin: 10px;
 `;
 
-/** 댓글 창 */
+/** 댓글 컨테이너 타입지정 */
 type CommentContainerProps = {
   visible: boolean;
 };
 
-/** 댓글창 */
+/** 댓글 컨테이너 스타일드 컴포넌트 */
 const CommentContainer = styled.div<CommentContainerProps>`
   width: 100%;
   display: ${(props) => (props.visible ? "block" : "none")};
 `;
 
-/**PostCommentsContainer 컴포넌트 */
+// Recoil atom 정의
+const commentCountState = atom({
+  key: "commentCount",
+  default: 0,
+});
+
+/** 게시물 댓글 컨테이너 컴포넌트 */
 const PostCommentsContainer = () => {
-  /** test 더미 데이터 */
   const post1 = {
-    isLike: true,
-    likedPersons: [
-      "김김",
-      "박박",
-      "이이이",
-      "정정정정",
-      "김김",
-      "박박",
-      "이이이",
-      "정정정정",
-    ],
+    likedPersons: [1234, 2345, 6326, 51252, 125, 4326, 2342, 543252],
   };
+  //로그인 된 유저 아이디 예시
+  const loginedId = 1234;
 
-  /** 댓글 개수 상태관리 */
-  const [commentCount, setCommentCount] = useState<number>(0); // commentCount 상태 추가
+  //좋아요 유무에 대한 변수
+  const isLiked = isLikedFunction(post1, loginedId);
 
-  /** 댓글 버튼 핸들러 클릭시 댓글창을 토글형식으로 랜더링 */
+  // Recoil 상태 및 업데이트 함수 가져오기
+  const [commentCount, setCommentCount] = useRecoilState(commentCountState);
+
+  /** 댓글 이미지 버튼 클릭 (토글) 핸들러 */
   const handleCommentImageClick = () => {
     setCommentContainerVisible(!commentContainerVisible);
   };
 
-  //초기값은 false로 댓글창을 숨김
+  // 댓글 컨테이너 시각 토글링 상태관리
   const [commentContainerVisible, setCommentContainerVisible] =
     useState<boolean>(false);
 
   return (
     <>
-      <div className="w-[100%] flex justify-between border-[1px] border-[gray] ">
+      <div className="w-[100%] flex justify-between border-[1px] border-[gray]">
         <div className="flex items-center">
           <button onClick={handleCommentImageClick}>
             <CommentImage src={"/comment.png"} alt="댓글 이미지" />
@@ -61,15 +63,22 @@ const PostCommentsContainer = () => {
             <span>댓글{commentCount}개</span>
           </div>
         </div>
+        {/* 게시글 좋아요 컴포넌트 */}
         <PostLikes
           likesCount={Number(post1.likedPersons.length)}
-          isLiked={post1.isLike}
+          isLiked={isLiked}
         />
       </div>
+      {/* 댓글 컨테이너 */}
       <CommentContainer visible={commentContainerVisible}>
-        <Comments />
+        {/* 댓글들 컴포넌트 */}
+        <Comments
+          commentCount={commentCount}
+          setCommentCount={setCommentCount}
+        />
       </CommentContainer>
     </>
   );
 };
+
 export default PostCommentsContainer;
